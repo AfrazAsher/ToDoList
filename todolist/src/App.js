0d746254f8c2b2f6ae11c9e5605b3d326fbaa8ea
unknown <afrazasher2@gmail.com>
2021-07-18 05:38:31 +0500

    New Commit

--- todolist/src/App.js
+++ todolist/src/App.js
@@ -1,14 +1,18 @@
-import React from "react";
+import React,{ useState } from "react";
 import './App.css';
 //Importing Components
 import Form from "./components/Form" 
+import TodoList from "./components/TodoList";
 function App() {
+  const [inputText,setInputText] = useState("");
+  const [todos,setTodos] = useState([]);
   return (
     <div className="App">
       <header>
         <h1>Afraz's ToDo List</h1>
       </header>
-      <Form/>
+      <Form inputText={inputText} todos={todos} setTodos={setTodos} setInputText={setInputText}/>
+      <TodoList todos={todos}/>
     </div>
   );
 }
---
+++ todolist/src/components/todolist.js
@@ -0,0 +1,14 @@
+import React from "react";
+// Import Components
+import Todo from "./Todo";
+const TodoList = () =>
+{
+  return(
+    <div className="todo-container">
+      <ul className="todo-list">
+        <Todo/>
+      </ul>
+    </div>
+  );
+};
+export default TodoList;
